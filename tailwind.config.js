281d65156f92b74100d26d9287549ee14dab1cc4
Alexey Abramenko <a.abramenko@chita.ru>
2022-06-26 15:31:59 +0900

    Add. Установка поддержки Vue и InertiaJS для взаимосвязи Vue и Laravel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
-  content: [
-      './storage/framework/views/*.php',
-      './resources/**/*.blade.php',
-      './resources/**/*.js',
-      './resources/**/*.vue'
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-      require('flowbite/plugin')
-  ],
-}
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './resources/js/**/*.vue',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+
+    plugins: [require('@tailwindcss/forms')],
+};
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,11 +12,11 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sourceMaps(!mix.inProduction(), 'source-map')
-    .js('resources/js/flowbite.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        require('tailwindcss')
-    ]);
+    .vue()
+    .postCss('resources/css/app.css', 'public/css', [require('tailwindcss'), require('autoprefixer')])
+    .alias({
+        '@': 'resources/js',
+    });
 
 if (mix.inProduction()) {
     mix.version();
