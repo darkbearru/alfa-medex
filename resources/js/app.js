fe0a7f1ad3bad35822e10357a76958e5b09b4389
Alexey Abramenko <a.abramenko@chita.ru>
2022-07-07 21:50:27 +0900

    Update. Настройка InertiaApp, а также добавление общего шаблона

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,20 +1,25 @@
 import './bootstrap';
 
-import { createApp, h } from 'vue';
-import { createInertiaApp } from '@inertiajs/inertia-vue3';
-import { InertiaProgress } from '@inertiajs/progress';
+import {createApp, h} from 'vue';
+import {createInertiaApp} from '@inertiajs/inertia-vue3';
+import {InertiaProgress} from '@inertiajs/progress';
+import AuthenticatedLayout from '@/Layouts/Authenticated.vue'
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => require(`./Pages/${name}.vue`),
-    setup({ el, app, props, plugin }) {
-        return createApp({ render: () => h(app, props) })
+    resolve: (name) => {
+        let page = require(`./Pages/${name}.vue`).default;
+        page.layout ??= AuthenticatedLayout;
+        return page;
+    },
+    setup({el, app, props, plugin}) {
+        return createApp({render: () => h(app, props)})
             .use(plugin)
-            .mixin({ methods: { route } })
+            .mixin({methods: {route}})
             .mount(el);
     },
 });
 
-InertiaProgress.init({ color: '#4B5563' });
+InertiaProgress.init({color: '#4B5563'});
