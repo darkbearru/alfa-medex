edc4b5ce1237ee0e6beb8f651a3259fc7c47fd44
Alexey Abramenko <a.abramenko@chita.ru>
2022-07-07 21:58:29 +0900

    Update. Изменение получения данных из каталога, также создание отдельного контроллера на работу с каталогом

--- app/Http/Controllers/CatalogController.php
+++ app/Http/Controllers/CatalogController.php
@@ -2,9 +2,34 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Catalog;
+use Illuminate\Database\Eloquent\Collection;
 
 class CatalogController extends Controller
 {
-    //
+    public static function Directories(int $parent = 0): array|Collection|null
+    {
+        return self::prepareDirectories(
+            Catalog::Directories($parent)
+        );
+    }
+
+    private static function prepareDirectories($recs): array
+    {
+        $result = [];
+        foreach ($recs as $rec) {
+            $new_rec = (object)[
+                "id" => $rec->id,
+                "name" => $rec->name,
+                "description" => $rec->description,
+                "children_count" => $rec->children_count,
+                "children" => []];
+            if (empty($result[$rec->parent_id])) {
+                $result[$rec->id] ??= $new_rec;
+            } else {
+                $result[$rec->parent_id]->children[$rec->id] ??= $new_rec;
+            }
+        }
+        return $result;
+    }
 }
