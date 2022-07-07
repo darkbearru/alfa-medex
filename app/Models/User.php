7a907b33029424113438553d1090ac8dc9cef45a
Alexey Abramenko <a.abramenko@chita.ru>
2022-07-07 21:56:26 +0900

    Update. Добавление разграничения прав доступа для пользователей

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,9 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    public static int $USER_RIGHT__ADMINISTRATOR = 16;
+    public static int $USER_RIGHT__SUPER_ADMIN = 32;
+
     /**
      * The attributes that are mass assignable.
      *
